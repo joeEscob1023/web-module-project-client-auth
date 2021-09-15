58f439ecba06589ea2c4182070881ef10cc3640a
Joe Escobedo <joe_escob1023@live.com>
2021-09-14 18:01:59 -0700

    authentication works, but Im having some issues making a list of friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import Login from "./Components/Login";
+import PrivateRoute from "./Components/PrivateRoute";
+import FriendsList from "./Components/FriendsList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+          <li>
+            {localStorage.getItem("token") && (
+              <div>
+                <Link to="/protected">Friends List</Link>
+              </div>
+            )}
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
